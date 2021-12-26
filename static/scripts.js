81917b1e364407375c3ffe43900d991c0922a59e
Yago Pajariño <yago977@hotmail.com>
2021-12-26 18:29:45 +0100

    scripts text into stats

--- static/scripts.js
+++ static/scripts.js
@@ -5,12 +5,14 @@ let actual = document.querySelector("#montoActual")
 var request = new XMLHttpRequest();
 request.open('GET', './info.txt', true);
 request.send(null);
-let text = request.responseText;
+request.addEventListener("load", useItems)
 
+function useItems(){
+    text = this.responseText
+    items = text.split(";")
+    console.log(items)
 
-items = text.split(";")
-console.log(items)
-
-operado.textContent = items[0]
-operaciones.textContent = items[1]
-actual.textContent = items[2]
+    operado.textContent = items[0]
+    operaciones.textContent = items[1]
+    actual.textContent = items[2]
+}
