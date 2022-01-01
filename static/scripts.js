cfbf0b2d67c7a937c864d4fe603edef2eb30ebda
Yago Pajariño <yago977@hotmail.com>
2022-01-01 13:50:39 +0100

    adds comisiones plot

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,9 @@
 let operado = document.querySelector("#operado")
 let operaciones = document.querySelector("#operaciones")
 let actual = document.querySelector("#montoActual")
+let comisiones = document.querySelector("#comisiones")
+let derechos = document.querySelector("#derechos")
+let iva = document.querySelector("#iva")
 
 var request = new XMLHttpRequest();
 request.open('GET', './info.txt', true);
@@ -15,4 +18,7 @@ function useItems(){
     operado.textContent = items[0]
     operaciones.textContent = items[1]
     actual.textContent = items[2]
+    comisiones.textContent = items[3]
+    derechos.textContent = items[4]
+    iva.textContent = items[5]
 }
